36b79cfc4c56d5affd523967db5dfac08560629e
ashab <ashab0007@icloud.com>
2026-08-11 03:05:42 +0530

    Initial commit

---
+++ src/main/java/com/example/mnnit_fc/MnnitFcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mnnit_fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnnitFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnnitFcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mnnit_fc/MnnitFcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnnit_fc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnnitFcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
